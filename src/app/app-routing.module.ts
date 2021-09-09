5ded5954ab466d178f19a9e50ae8d8d13a38458a
Tijana Rosic <rosictijana95@gmail.com>
2021-09-09 19:57:44 +0200

    Zapocet prikaz svih poslovnih banaka

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PoslovneBankeComponent } from './components/poslovne-banke/poslovne-banke.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: PoslovneBankeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
