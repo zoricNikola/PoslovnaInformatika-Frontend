a7df6b862b182a41348abaebdf58d4f0a287a6f0
Boris <jankovicb0230@gmail.com>
2021-09-10 10:32:19 +0200

    Kreiran confirmation dialog i dialog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,17 @@ import { AppComponent } from './app.component';
 import { PoslovneBankeComponent } from './components/poslovne-banke/poslovne-banke.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { AccordionComponent } from './common/accordion/accordion.component';
+import { ConfirmationDialogComponent } from './common/confirmation-dialog/confirmation-dialog.component';
+import { DialogComponent } from './common/dialog/dialog.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PoslovneBankeComponent,
     NavbarComponent,
-    AccordionComponent
+    AccordionComponent,
+    ConfirmationDialogComponent,
+    DialogComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/common/confirmation-dialog/confirmation-dialog.component.ts
@@ -0,0 +1,24 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+export interface ConfirmationDialogOptions {
+  title: string;
+  message: string;
+  decline: () => void;
+  confirm: () => void;
+}
+
+@Component({
+  selector: 'confirmation-dialog',
+  templateUrl: './confirmation-dialog.component.html',
+  styleUrls: ['./confirmation-dialog.component.css'],
+})
+
+export class ConfirmationDialogComponent implements OnInit {
+  @Input('opened') opened: boolean = false;
+  @Input('options') options!: ConfirmationDialogOptions;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
